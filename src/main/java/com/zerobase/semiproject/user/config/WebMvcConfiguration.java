a5b4adfb46c5333f6ed75b2b58032d9cdef213dc
Yangdoubleh <why374@naver.com>
2024-06-07 08:58:02 +0900

    Feat(#v0.0.3) : ADD CORS CONFIG

---
+++ src/main/java/com/zerobase/semiproject/user/config/WebMvcConfiguration.java
@@ -0,0 +1,26 @@
+package com.zerobase.semiproject.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfiguration {
+
+    private final List<String> ALLOW_DOMAIN_LIST = List.of("http://webserver1.com", "http://webserver2.com");
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                ALLOW_DOMAIN_LIST.forEach(ALLOW_DOMAIN -> {
+                    registry.addMapping("/**").allowedOrigins(ALLOW_DOMAIN);
+                });
+            }
+        };
+    }
+}
